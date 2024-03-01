3b668726db94572db57194a6187f82b1dcd29151
Rafik Hijazy <rafikhz90@gmail.com>
2024-03-01 08:47:52 +0200

    main.js Add function

--- js/main.js
+++ js/main.js
@@ -1,30 +1,62 @@
-const tasks= localStorage.getItem("tasks");
+let tasks= localStorage.getItem("tasks");
 const add_btn=document.querySelector("#add-btn");
 const text_field=document.querySelector("#task-field");
 
+console.log(tasks.tasks)
 
+//localStorage.removeItem("tasks")
 //const tasks=['<li class="task flex-items Gap"><h3 class="task-text">Task</h3><h4 class="task-date">11:11:1111</h4><a type="button" class="done-bt" href="" onClick="done()">👍🏼</a><a type="button" class="remove-bt" href="">🗑</a></li>']
-function checkArray(tasks){
+function checkArray(){
     if(tasks===null){
-        console.log("fytf")
-        tasks=localStorage.setItem("tasks",JSON.stringify('{"tasks":[]}'))
+        
+        tasks=localStorage.setItem("tasks",JSON.stringify({"tasks":[]}))
+    }
+    else{
+        console.log('done')
+        
+         tasks=JSON.parse(tasks)
+         fillTaskList(tasks)
+         //tasks.tasks
+         console.log(tasks.tasks)
+        
     }
     
 }
 
-checkArray(tasks)
+//checkArray()
+console.log(tasks)
+
 function fillTaskList(tasks){
     const task_list=document.querySelector('#task-list')
-    for (let i=0 ; i<tasks.length;i++){
-        task_list.innerHTML+=tasks[i]
+    for (let i=0 ; i<tasks.tasks.length;i++){
+        task_list.innerHTML+=tasks.tasks[i]
     }
     //task_list.innerHTML+='<li class="task flex-items Gap"><h3 class="task-text">Task</h3><h4 class="task-date">11:11:1111</h4><a type="button" class="done-bt" href="" onClick="done(this.PannerNode)">👍🏼</a><a type="button" class="remove-bt" href="" onClick="remove(this.PannerNode)">🗑</a></li>'
 
 }
-fillTaskList(tasks)
+//fillTaskList(tasks)
+function checkTextValidation(text){
+    return text!=''
+}
 
-function done(){
+add_btn.addEventListener('click',function(){
+    newText=text_field.value;
+    const new_task=document.createElement("li");
+    if(checkTextValidation){
+        new_task.innerHTML=`<li class="task flex-items Gap"><h3 class="task-text">${newText}</h3><h4 class="task-date">${new Date()}</h4><a type="button" class="done-bt" href="" onClick="done()">👍🏼</a><a type="button" class="remove-bt" href="">🗑</a></li>`;
+        
+        document.querySelector("#task-list").appendChild(new_task)
+    }
+    else{
+        alert("Empty text is not valid!!");
+    }
+
+})
+function done(item){
    alert('done')
+}
+function remove(item){
+
 }
 //fillTaskList(tasks)
 /** <li class="task flex-items Gap">
