118c36220f309fa36c9b9ac4587d9af1151a94d8
Rafik Hijazy <rafikhz90@gmail.com>
2024-03-16 13:36:22 +0200

    I have take the permition:put a comment about my queries error

--- js/index.js
+++ js/index.js
@@ -1,15 +1,39 @@
 
-const log_button = document.querySelector('#login-form')
+const log_button = document.querySelector('#loger')
 const User="AdminSEF123"
 const Password="SeF@ctORy$$456"
 
 
-
+/** this error keep appear for me when i use my queries methods :
+ *  POST http://127.0.0.1:5501/todo-frontend/Assignment-3-FSW/localhost/Todo/phpscripts/signin.php net::ERR_ABORTED 405 (Method Not
+ * i know her must receive a JSON object and continue my process as usual */
 
 log_button.addEventListener('click',function(){
-    const user=document.querySelector('#user').value
+    const email=document.querySelector('#user').value
     const password=document.querySelector('#password').value
-    if(User !=  user.trim()){
+    const formData=new FormData();
+    try{
+    const signin=async (email,password)=>{
+        formData.append('email',email);
+        formData.append('password',password)
+        const response=await fetch(`localhost/Todo/phpscripts/signin.php/`,{
+            method:"POST",
+           
+            body:formData
+            
+        })
+    
+    return await response
+    }
+    const ss=  signin(email,password);
+    console.log(ss);
+
+}
+    catch(err){
+        console.log(err);
+    }
+    //console.log(signin)
+   /* if(User !=  user.trim()){
         const error=document.querySelector('#user-err');
         error.style.display=block;
         
@@ -21,5 +45,5 @@ log_button.addEventListener('click',function(){
         
         window.location.href='./pages/main.html'
         
-    }
+    }*/
 })
